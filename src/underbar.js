066bd4cb74ebe98448c5a7a0e79426120d9c599d
Brian Scoles <brianscoles@gmail.com>
2014-10-03 17:35:25 -0700

    added a "_.sort" that sorts arrays of primitives, via quicksort.
    this can next be templatized and become _.sortBy.

--- src/underbar.js
+++ src/underbar.js
@@ -370,6 +370,59 @@ var _ = {};
   _.sortBy = function(collection, iterator) {
   };
 
+  _.sort = function(collection){
+
+    //this will be an implementation of quicksort.  
+    //first: choose a pivot value.
+    function getPivotIndex(start, stop){
+      return Math.floor(Math.random()*(stop-start+1)+start);
+      //return start;
+    }
+
+    function swap(collection, a, b){
+      //debug("swapping " + collection[a] + " and " + collection[b]);
+
+      var temp = collection[a];
+      collection[a]=collection[b];
+      collection[b]=temp;
+      //debug(collection); 
+    }
+
+    function sortSubsection(collection, start, stop){
+      var pivotIndex = getPivotIndex(start, stop);
+      var cursorIndex = start;
+      var pivotValue = collection[pivotIndex];
+      //debug("Now sorting subsection of elements "+ start + " to " + stop + " with pivot value " + pivotValue);
+      //debug(collection);
+      
+      swap(collection, pivotIndex, stop);
+      for (var i = start; i < stop; i++) {
+        if(collection[i]<pivotValue){
+          swap(collection, i, cursorIndex);
+          cursorIndex += 1;
+        } //else {
+          //debug("NOT swapping "+collection[i] + " and " + collection[cursorIndex] + " because " + collection[i] + " is not smaller than " + pivotValue);
+        //}
+      }
+      //debug("Done with with subsection call... putting the pivot in its place.");
+      swap(collection, stop, cursorIndex);
+      //debug(" ");
+      
+      if(cursorIndex - start>1){
+        sortSubsection(collection,start,cursorIndex-1);
+      }
+      if(stop - cursorIndex >1){
+        sortSubsection(collection,cursorIndex+1,stop);
+      }
+    }
+
+    sortSubsection(collection,0,collection.length-1);
+    return collection;
+
+  };
+
+
+
   // Zip together two or more arrays with elements of the same index
   // going together.
   //
